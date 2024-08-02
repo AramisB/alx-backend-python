3424146d43cc2aef53717cf6dd7d50ef044aabaf
AramisB <brendachem7@gmail.com>
2024-08-02 15:41:27 +0300

    return a callable function

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -10,7 +10,7 @@ def make_multiplier(mult: float) -> Callable[[float], float]:
     A type-annotated function make_multiplier that takes a float multiplier
     as argument and returns a function that multiplies a float by multiplier
     """
-    def multiplier(n: float) -> float:
+    def multiplier(n: float):
         """
         A type-annotated function multiplier
         """
