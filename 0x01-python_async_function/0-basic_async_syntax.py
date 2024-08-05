a44a6b6f503c0efdc656e207d3e801d2fe44d72b
AramisB <brendachem7@gmail.com>
2024-08-05 12:49:40 +0100

    generated random floating point numbers using random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -17,4 +17,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Return: The float value of the random delay
     Use the random module.
     """
-    return random.uniform(0, max_delay)
+    sleep_time = random.uniform(0, max_delay)
+    await asyncio.sleep(sleep_time)
+    return sleep_time
